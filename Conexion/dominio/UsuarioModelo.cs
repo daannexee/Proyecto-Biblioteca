6eaafadbef7d38923c3b4d7667806fbd037af6bf
Emerson <emerprado16@gmail.com>
2025-10-08 15:21:43 -0600

    Add files via upload

---
+++ Conexion/dominio/UsuarioModelo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acceso_Datos;
+
+namespace dominio
+{
+    public class UsuarioModelo : Conexcion_a_BaseDatos
+    {
+        private readonly UsuarioModelo usuarioDao = new UsuarioModelo();
+        public bool LoginUsuario(string Numero_Identificacion, string Contraseña)
+        {
+            return usuarioDao.LoginUsuario(Numero_Identificacion, Contraseña);
+        }
+    }
+}
