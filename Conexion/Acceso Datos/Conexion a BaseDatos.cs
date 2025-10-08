625e41ed3f17e6916131715cda06d90240ad758d
Emerson <emerprado16@gmail.com>
2025-10-08 15:20:41 -0600

    Add files via upload

---
+++ Conexion/Acceso Datos/Conexion a BaseDatos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Acceso_Datos
+{
+    public abstract class Conexcion_a_BaseDatos
+    {
+        private readonly string conection;
+
+        public Conexcion_a_BaseDatos() {
+            conection = "server=localhost;user=root;database=biblioteca;password=root";
+        }
+
+        protected MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(conection);
+        }
+    }
+}
---
+++ Conexion/Acceso Datos/UsuarioDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace Acceso_Datos
+{
+    internal class UsuarioDao : Conexcion_a_BaseDatos
+    {
+        public bool Login(string Numero_Identificacion, string Contraseña) 
+        {
+            using (var conection = GetConnection())
+            {
+                conection.Open();
+                using (var command = new MySqlCommand())
+                {
+                    command.Connection = conection;
+                    command.CommandText = "SELECT COUNT(*) FROM usuarios WHERE Numero_Identificacion = @Numero_Identificacion AND Contraseña = @Contraseña";
+                    command.Parameters.AddWithValue("@Numero_Identificacion", Numero_Identificacion);
+                    command.Parameters.AddWithValue("@Contraseña", Contraseña);
+                    command.CommandType = System.Data.CommandType.Text;                   
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
